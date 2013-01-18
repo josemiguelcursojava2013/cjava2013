59fd871515624bd0256c42fc878cd8cea2453f19
Ignacio G <Ignacio.espacio@gmail.com>
2013-01-18 13:56:35 +0100

    cambios clases Asignaturas y Alumnos

--- colecciones/src/main/java/curso/colecciones/Alumno.java
+++ colecciones/src/main/java/curso/colecciones/Alumno.java
@@ -6,35 +6,44 @@ package curso.colecciones;
 
 import java.util.Set;
 import java.util.TreeSet;
+
 /**
  *
  * @author Usuario
  */
 public class Alumno {
+
     private int id;
     private String nombre;
-    private Set <Asignatura> misAsignaturas;
-    Alumno(int id,String nombre){
-        this.id=id;
-        this.nombre=nombre;
-        misAsignaturas=new TreeSet<Asignatura>();
+    private Set<Asignatura> misAsignaturas;
+
+    Alumno(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+        misAsignaturas = new TreeSet<Asignatura>();
     }
-    public void setId(int id){
-        this.id=id;
+
+    public void setId(int id) {
+        this.id = id;
     }
-    public void setNombre(String nombre){
-        this.nombre=nombre;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
-    public int getId(){
+
+    public int getId() {
         return id;
     }
-    public String nombre(){
+
+    public String nombre() {
         return nombre;
     }
-    public void addAsignatura(Asignatura asignatura){
+
+    public void addAsignatura(Asignatura asignatura) {
         misAsignaturas.add(asignatura);
     }
-    public void removeAsignatura(Asignatura asignatura){
+
+    public void removeAsignatura(Asignatura asignatura) {
         misAsignaturas.remove(asignatura);
     }
 }
--- colecciones/src/main/java/curso/colecciones/Asignatura.java
+++ colecciones/src/main/java/curso/colecciones/Asignatura.java
@@ -13,6 +13,7 @@ import java.util.TreeSet;
  * @author Usuario
  */
 public class Asignatura {
+
     private int id;
     private String nombre;
     private Profesor myProfesor;
@@ -20,19 +21,56 @@ public class Asignatura {
     private int horas;
     private Date inicio;
     private Date fin;
-    private Set <Alumno> misAlumnos;
-    
-    Asignatura(int id,String nombre,Profesor myProfesor,Aula myAula,int horas,Date inicio,Date fin){
-        this.id=id;
-        this.nombre=nombre;
-        this.myAula=myAula;
-        this.myProfesor=myProfesor;
-        this.horas=horas;
-        this.inicio=inicio;
-        this.fin=fin;
-        misAlumnos=new TreeSet <Alumno>();
+    private Set<Alumno> misAlumnos;
+
+    Asignatura(int id, String nombre, Profesor myProfesor, Aula myAula, int horas, Date inicio, Date fin) {
+        this.id = id;
+        this.nombre = nombre;
+        this.myAula = myAula;
+        this.myProfesor = myProfesor;
+        this.horas = horas;
+        this.inicio = inicio;
+        this.fin = fin;
+        misAlumnos = new TreeSet<Alumno>();
     }
-    public void addAlumno(Alumno myAlumno){
+
+    public void addAlumno(Alumno myAlumno) {
         misAlumnos.add(myAlumno);
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+    public void setProfesor(Profesor myProfesor){
+        this.myProfesor=myProfesor;
+    }
+    public void setHoras(int horas){
+        this.horas=horas;
+    }
+    public void setInicio(Date inicio){
+        this.inicio=inicio;
+    }
+    public void setFin(Date fin){
+        this.fin=fin;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public Profesor getProfesor(){
+        return myProfesor;
+    }
+    public int getHoras(){
+        return horas;
+    }
+    public Date setInicio(){
+        return inicio;
+    }
+    public Date setFin(){
+        return fin;
     }
 }
