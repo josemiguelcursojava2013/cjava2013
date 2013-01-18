2d62e1cac104d4ef73e0efe92575ec50f6df0937
Ignacio G <Ignacio.espacio@gmail.com>
2013-01-18 13:45:45 +0100

    cambio nuevo

--- colecciones/src/main/java/curso/colecciones/Asignatura.java
+++ colecciones/src/main/java/curso/colecciones/Asignatura.java
@@ -13,23 +13,26 @@ import java.util.TreeSet;
  * @author Usuario
  */
 public class Asignatura {
-    int id;
-    String nombre;
-    Profesor myProfesor;
-    Aula myAula;
-    int horas;
-    Date inicio;
-    Date fin;
-    Set <Alumno> misAlumnos;
+    private int id;
+    private String nombre;
+    private Profesor myProfesor;
+    private Aula myAula;
+    private int horas;
+    private Date inicio;
+    private Date fin;
+    private Set <Alumno> misAlumnos;
     
     Asignatura(int id,String nombre,Profesor myProfesor,Aula myAula,int horas,Date inicio,Date fin){
         this.id=id;
         this.nombre=nombre;
         this.myAula=myAula;
-        this.myProfesor=new Profesor;
-        this.horas=new horas;
+        this.myProfesor=myProfesor;
+        this.horas=horas;
         this.inicio=inicio;
         this.fin=fin;
         misAlumnos=new TreeSet <Alumno>();
+    }
+    public void addAlumno(Alumno myAlumno){
+        misAlumnos.add(myAlumno);
     }
 }
